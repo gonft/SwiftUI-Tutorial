bc8cd25ae95648dd3ac791a329f6c42641afd8af
Paul Han <proghjy@gmail.com>
2019-09-24 12:07:48 +0900

    init project

---
+++ SwiftUI-Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Paul Han on 2019/09/24.
+//  Copyright © 2019 Paul Han. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

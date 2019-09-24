cf0853fc4c212bd7d853443254a90ae6118641ae
Paul Han <proghjy@gmail.com>
2019-09-24 12:20:45 +0900

    #1 Add text to view detail information

---
+++ SwiftUI-Tutorial/UI/LandmarkDetail.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkDetail.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Paul Han on 2019/09/24.
+//  Copyright © 2019 Paul Han. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}

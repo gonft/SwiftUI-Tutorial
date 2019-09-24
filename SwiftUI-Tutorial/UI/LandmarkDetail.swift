3e559d266346e36e257b9e2eeb74562b6556355c
Paul Han <proghjy@gmail.com>
2019-09-24 13:05:00 +0900

    #2 Pass Data into Child Views

--- SwiftUI-Tutorial/UI/LandmarkDetail.swift
+++ SwiftUI-Tutorial/UI/LandmarkDetail.swift
@@ -9,13 +9,16 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    
+    var landmark: Landmark
+    
     var body: some View {
         VStack {
-            MapView()
+            MapView(coordinate: landmark.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
 
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
 
@@ -39,6 +42,6 @@ struct LandmarkDetail: View {
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarkData[0])
     }
 }

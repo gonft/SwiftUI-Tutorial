60454e152e07c913265d16f0ce73b20bd4e147fd
Paul Han <proghjy@gmail.com>
2019-09-24 12:54:24 +0900

    #2 Create the List of Landmarks

---
+++ SwiftUI-Tutorial/UI/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Paul Han on 2019/09/24.
+//  Copyright © 2019 Paul Han. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}

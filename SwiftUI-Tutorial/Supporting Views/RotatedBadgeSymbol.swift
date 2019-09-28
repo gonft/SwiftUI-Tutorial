9c448f969e59d4829cf3b88833b9b60e2b7089d5
Paul Han <proghjy@gmail.com>
2019-09-28 15:46:21 +0800

    #4 Combine the Badge Foreground and Background

---
+++ SwiftUI-Tutorial/Supporting Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Paul Han on 2019/09/28.
+//  Copyright © 2019 Paul Han. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}

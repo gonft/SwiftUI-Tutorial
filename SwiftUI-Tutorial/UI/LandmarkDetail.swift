9dc8d08d26b3c4fde0d0005f754c50a85a75d418
Paul Han <proghjy@gmail.com>
2019-09-28 14:57:32 +0800

    #3 Create a Favorite Button for Each Landmark

--- SwiftUI-Tutorial/UI/LandmarkDetail.swift
+++ SwiftUI-Tutorial/UI/LandmarkDetail.swift
@@ -29,13 +29,27 @@ struct LandmarkDetail: View {
                     .padding(.bottom, -130)
 
                 VStack(alignment: .leading) {
-                    Text("Turtle Rock")
-                        .font(.title)
+                    HStack {
+                        Text(landmark.name)
+                            .font(.title)
+                        
+                        Button(action: {
+                            self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
+                        }) {
+                            if self.userData.landmarks[self.landmarkIndex].isFavorite {
+                                Image(systemName: "star.fill")
+                                    .foregroundColor(Color.yellow)
+                            } else {
+                                Image(systemName: "star")
+                                    .foregroundColor(Color.gray)
+                            }
+                        }
+                    }
                     HStack(alignment: .top) {
-                        Text("Joshua Tree National Park")
+                        Text(landmark.park)
                             .font(.subheadline)
                         Spacer()
-                        Text("California")
+                        Text(landmark.state)
                             .font(.subheadline)
                     }
                 }
@@ -50,5 +64,6 @@ struct LandmarkDetail: View {
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkDetail(landmark: landmarkData[0])
+        .environmentObject(UserData())
     }
 }

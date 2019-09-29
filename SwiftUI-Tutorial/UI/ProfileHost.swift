ea6644ed99afd8ae6eb8d5c3feefd963c924d936
Paul Han <proghjy@gmail.com>
2019-09-29 11:15:20 +0800

    #8 Display a User Profile

---
+++ SwiftUI-Tutorial/UI/ProfileHost.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileHost.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Paul Han on 2019/09/29.
+//  Copyright © 2019 Paul Han. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @Environment(\.editMode) var mode
+    @EnvironmentObject var userData: UserData
+    @State var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Spacer()
+                
+                EditButton()
+            }
+            if self.mode?.wrappedValue == .inactive {
+                ProfileSummary(profile: userData.profile)
+            } else {
+                Text("Profile Editor")
+            }
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}

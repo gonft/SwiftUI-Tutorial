2b74c7bc426f97d6d5f2ba5b5fb74ab2f7f0bad7
Paul Han <proghjy@gmail.com>
2019-09-29 15:53:53 +0800

    #6 Create a Custom Notification Interface

--- WatchLandmarks Extension/ContentView.swift
+++ WatchLandmarks Extension/ContentView.swift
@@ -11,13 +11,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         LandmarkList { WatchLandmarkDetail(landmark: $0) }
-        .environmentObject(UserData())
+            .environmentObject(UserData())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkList { WatchLandmarkDetail(landmark: $0) }
-        .environmentObject(UserData())
+            .environmentObject(UserData())
     }
 }

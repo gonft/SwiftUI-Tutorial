fd124b5b10cb3a271b4f805a517060f09a9c62b2
Paul Han <proghjy@gmail.com>
2019-09-29 12:46:28 +0800

    #7 Track the Page in a SwiftUI View’s State

---
+++ SwiftUI-Tutorial/UI/Home/PageView/PageControl.swift
@@ -0,0 +1,26 @@
+//
+//  PageControl.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Paul Han on 2019/09/29.
+//  Copyright © 2019 Paul Han. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+
+        return control
+    }
+
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+}
